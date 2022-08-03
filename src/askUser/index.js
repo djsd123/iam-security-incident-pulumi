8222cf0ecb9eacb297ad1c793678f3105c910e07
Mikael Allison <mikellallison@gmail.com>
2022-08-03 11:28:38 +0100

    link

--- src/askUser/index.js
+++ src/askUser/index.js
@@ -4,9 +4,12 @@ let sns = new AWS.SNS()
 
 exports.handler = async (event, context) => {
 
+    let allow = `${process.env.APIAllowEndpoint + '?token=' + JSON.stringify(event.token)}`
+    let deny = `${process.env.APIDenyEndpoint + '?token=' + JSON.stringify(event.token)}`
+
     let params = {
         TopicArn: process.env.TOPIC,
-        Message: 'A restricted Policy change has been detected Approve: ' + process.env.APIAllowEndpoint + '?token=' + JSON.stringify(event.token) + ' \n\nOr Deny: ' + process.env.APIDenyEndpoint + '?token=' + JSON.stringify(event.token)
+        Message: 'A restricted Policy change has been detected \n\nApprove: ' + allow + ' \n\nOr Deny: ' + deny
     }
 
     try {
